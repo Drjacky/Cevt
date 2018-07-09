1b5bc8e29a01e3c7788154a0d152ccd9e33d0b9c
Drjacky <dr.jacky.2005@gmail.com>
2018-07-09 19:38:08 +0430

    Create ApiService.

---
+++ app/src/main/java/ir/hosseinabbasi/cevt/data/network/ApiService.kt
@@ -0,0 +1,14 @@
+package ir.hosseinabbasi.cevt.data.network
+
+import retrofit2.Retrofit
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/**
+ * Created by Dr.jacky on 7/9/2018.
+ */
+@Singleton
+class ApiService @Inject
+constructor(retrofit: Retrofit) : IApiService{
+    private val mRetrofit = retrofit
+}
---
+++ app/src/main/java/ir/hosseinabbasi/cevt/data/network/IApiService.kt
@@ -0,0 +1,6 @@
+package ir.hosseinabbasi.cevt.data.network
+
+/**
+ * Created by Dr.jacky on 7/9/2018.
+ */
+interface IApiService
