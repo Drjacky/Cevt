5668d6f8a9743a8f1576c1bb71d5473d582a25e5
Drjacky <dr.jacky.2005@gmail.com>
2018-07-09 22:27:57 +0430

    Create IMainActivityPresenter.

---
+++ app/src/main/java/ir/hosseinabbasi/cevt/ui/main/IMainActivityPresenter.kt
@@ -0,0 +1,8 @@
+package ir.hosseinabbasi.cevt.ui.main
+
+import ir.hosseinabbasi.cevt.ui.base.IBasePresenter
+
+/**
+ * Created by Dr.jacky on 7/9/2018.
+ */
+interface IMainActivityPresenter<V : IMainActivityView, I : IMainActivityInteractor> : IBasePresenter<V, I>
