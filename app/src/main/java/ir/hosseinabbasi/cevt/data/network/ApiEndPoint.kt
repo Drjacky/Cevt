9c56cfa2adc9c55051896ec553dae7740837b474
Drjacky <dr.jacky.2005@gmail.com>
2018-07-09 19:36:04 +0430

    Add API KEY to BuildConfig and ApiEndPoint.

---
+++ app/src/main/java/ir/hosseinabbasi/cevt/data/network/ApiEndPoint.kt
@@ -0,0 +1,13 @@
+package ir.hosseinabbasi.cevt.data.network
+
+import ir.hosseinabbasi.cevt.BuildConfig
+
+/**
+ * Created by Dr.jacky on 7/9/2018.
+ */
+class ApiEndPoint {
+    companion object {
+        const val ENDPOINT_CEVT = BuildConfig.BASE_URL
+        const val API_KEY = BuildConfig.API_KEY
+    }
+}
