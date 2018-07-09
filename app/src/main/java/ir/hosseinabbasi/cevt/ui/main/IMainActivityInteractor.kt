10cf86c6918f0ba03d32eabcd9f4916e05421e0f
Drjacky <dr.jacky.2005@gmail.com>
2018-07-09 22:27:12 +0430

    Create IMainActivityInteractor.

---
+++ app/src/main/java/ir/hosseinabbasi/cevt/ui/main/IMainActivityInteractor.kt
@@ -0,0 +1,8 @@
+package ir.hosseinabbasi.cevt.ui.main
+
+import ir.hosseinabbasi.cevt.ui.base.IBaseInteractor
+
+/**
+ * Created by Dr.jacky on 7/9/2018.
+ */
+interface IMainActivityInteractor : IBaseInteractor
