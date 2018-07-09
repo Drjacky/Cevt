d42a53645e5801581d9a2f192301b4b72790b16d
Drjacky <dr.jacky.2005@gmail.com>
2018-07-09 20:44:27 +0430

    Create DI Qualifires.

---
+++ app/src/main/java/ir/hosseinabbasi/cevt/di/qualifier/ActivityContext.kt
@@ -0,0 +1,10 @@
+package ir.hosseinabbasi.cevt.di.qualifier
+
+import javax.inject.Qualifier
+
+/**
+ * Created by Dr.jacky on 7/9/2018.
+ */
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ActivityContext
---
+++ app/src/main/java/ir/hosseinabbasi/cevt/di/qualifier/PerActivity.kt
@@ -0,0 +1,10 @@
+package ir.hosseinabbasi.cevt.di.qualifier
+
+import javax.inject.Scope
+
+/**
+ * Created by Dr.jacky on 7/9/2018.
+ */
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class PerActivity
