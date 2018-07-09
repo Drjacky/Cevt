98c7272fe71f27699844be56d9a3defeedafc4ec
Drjacky <dr.jacky.2005@gmail.com>
2018-07-09 20:47:53 +0430

    Create ActivityComponent.

---
+++ app/src/main/java/ir/hosseinabbasi/cevt/di/component/ActivityComponent.kt
@@ -0,0 +1,11 @@
+package ir.hosseinabbasi.cevt.di.component
+
+import dagger.Component
+import ir.hosseinabbasi.cevt.di.qualifier.PerActivity
+
+/**
+ * Created by Dr.jacky on 7/9/2018.
+ */
+@PerActivity
+@Component(dependencies = arrayOf(ApplicationComponent::class), modules = arrayOf())
+interface ActivityComponent
