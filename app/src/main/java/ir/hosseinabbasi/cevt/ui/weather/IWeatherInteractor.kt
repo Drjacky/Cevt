4b4d9af3f9029c31379ca80392d30d73a3a59c61
Dr.jacky <dr.jacky.2005@gmail.com>
2018-07-14 00:16:52 +0430

    Create IWeatherInteractor.

---
+++ app/src/main/java/ir/hosseinabbasi/cevt/ui/weather/IWeatherInteractor.kt
@@ -0,0 +1,8 @@
+package ir.hosseinabbasi.cevt.ui.weather
+
+import ir.hosseinabbasi.cevt.ui.base.IBaseInteractor
+
+/**
+ * Created by Dr.jacky on 7/14/2018.
+ */
+interface IWeatherInteractor : IBaseInteractor
