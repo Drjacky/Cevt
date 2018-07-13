e942b0842af446e2f619e1a36f972484e3a02735
Dr.jacky <dr.jacky.2005@gmail.com>
2018-07-14 00:18:15 +0430

    Create IWeatherPresenter.

---
+++ app/src/main/java/ir/hosseinabbasi/cevt/ui/weather/IWeatherPresenter.kt
@@ -0,0 +1,8 @@
+package ir.hosseinabbasi.cevt.ui.weather
+
+import ir.hosseinabbasi.cevt.ui.base.IBasePresenter
+
+/**
+ * Created by Dr.jacky on 7/14/2018.
+ */
+interface IWeatherPresenter<V: IWeatherView, I: IWeatherInteractor> : IBasePresenter<V, I>
